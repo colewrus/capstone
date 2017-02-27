a6e929b6100e5d8b432ca3397d075e1e195f4b23
colewrus <wrampelc@gmail.com>
2017-02-27 09:49:49 -0500

    first commit

---
+++ Assets/Sets/Feb 2017/unitTest_1.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class unitTest_1 : MonoBehaviour {
+
+
+	public List<float> energy_1x3 = new List<float>();
+	public List<float> materials_1x3 = new List<float>();
+	public List<float> labor_1x3 = new List<float>();
+	public matrixClass[] identityMatrix;
+
+
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < identityMatrix.Length; i++) {
+			for (int j = 0; j < identityMatrix.Length; j++) {
+				print (identityMatrix [i].Matrix[j]);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+		
+}
