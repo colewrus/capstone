58b6a47edd27f374130a67329210ddfa5d6e14e5
colewrus <wrampelc@gmail.com>
2017-03-21 13:59:23 -0400

    possibly fixed the damn UI resizing issue

--- Assets/Sets/Feb 2017/unit3_GUI/GUIM.cs
+++ Assets/Sets/Feb 2017/unit3_GUI/GUIM.cs
@@ -7,13 +7,14 @@ public class GUIM : MonoBehaviour {
 	
 
 	public GameObject infoPanel;//our main panel;
+	public List<GameObject> panel_Info_buttons = new List<GameObject>();
 	public string[] buttonPos;  //!------------------ Array -------- DEBORAAAHHHHHHHH
 	GameObject nameHold;
 	public GameObject laborerFocus;
 
 	// Use this for initialization
 	void Start () {
-	
+		infoPanel.GetComponent<UIController> ().Public_Hide ();	
 	}
 	
 	// Update is called once per frame
@@ -25,6 +26,7 @@ public class GUIM : MonoBehaviour {
 		if (nameHold == null) {
 			nameHold = EventSystem.current.currentSelectedGameObject.gameObject;
 			Camera.main.transform.position = new Vector3 (laborerFocus.transform.position.x-3, laborerFocus.transform.position.y, Camera.main.transform.position.z);
+			button_Manger ();
 		}
 
 		if (infoPanel.activeSelf == true) {
@@ -34,6 +36,7 @@ public class GUIM : MonoBehaviour {
 				nameHold = null;
 			} else {
 				Invoke ("ReOpen", 0.5f);
+				button_Manger ();
 				nameHold = EventSystem.current.currentSelectedGameObject.gameObject;
 			}
 
@@ -53,17 +56,39 @@ public class GUIM : MonoBehaviour {
 
 
 	void button_Manger(){
+		
+		//disable all the button specific info panels
+		for (int i = 0; i < panel_Info_buttons.Count; i++) { 
+			panel_Info_buttons [i].gameObject.SetActive (false);
+		}
+
+		//Money
+		if (EventSystem.current.currentSelectedGameObject.name == "Money") {
+			panel_Info_buttons [0].gameObject.SetActive (true);
+		}
+
 		//Energy
+		if (EventSystem.current.currentSelectedGameObject.name == "e") {
+			panel_Info_buttons [1].gameObject.SetActive (true);
+		}
 
 		//Materials
+		if (EventSystem.current.currentSelectedGameObject.name == "m") {
+			panel_Info_buttons [2].gameObject.SetActive (true);
+		}
 
 		//Employees
-
+		if (EventSystem.current.currentSelectedGameObject.name == "l") {
+			panel_Info_buttons [3].gameObject.SetActive (true);
+		}
 		//Upgrades
-
+		if (EventSystem.current.currentSelectedGameObject.name == "u") {
+			panel_Info_buttons [4].gameObject.SetActive (true);
+		}
 		//Bill of Sale
-		// Carousel for item
-		// Object Cost
-		//Total Cost area
+		if (EventSystem.current.currentSelectedGameObject.name == "b") {
+			panel_Info_buttons [5].gameObject.SetActive (true);
+		}
+
 	}
 }
