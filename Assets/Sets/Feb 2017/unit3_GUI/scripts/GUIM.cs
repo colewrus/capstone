173787a01c0133f39df9f08b9209ab8f5523da34
colewrus <wrampelc@gmail.com>
2017-03-22 10:09:59 -0400

    started tutorial

--- Assets/Sets/Feb 2017/unit3_GUI/scripts/GUIM.cs
+++ Assets/Sets/Feb 2017/unit3_GUI/scripts/GUIM.cs
@@ -7,14 +7,15 @@ public class GUIM : MonoBehaviour {
 	
 
 	public GameObject infoPanel;//our main panel;
-	public List<GameObject> panel_Info_buttons = new List<GameObject>();
+	public List<GameObject> buttons_info = new List<GameObject>();
 	public string[] buttonPos;  //!------------------ Array -------- DEBORAAAHHHHHHHH
 	GameObject nameHold;
+	GameObject infoHold;
 	public GameObject laborerFocus;
 
 	// Use this for initialization
 	void Start () {
-		infoPanel.GetComponent<UIController> ().Public_Hide ();	
+	
 	}
 	
 	// Update is called once per frame
@@ -26,7 +27,7 @@ public class GUIM : MonoBehaviour {
 		if (nameHold == null) {
 			nameHold = EventSystem.current.currentSelectedGameObject.gameObject;
 			Camera.main.transform.position = new Vector3 (laborerFocus.transform.position.x-3, laborerFocus.transform.position.y, Camera.main.transform.position.z);
-			button_Manger ();
+			button_Manager ();
 		}
 
 		if (infoPanel.activeSelf == true) {
@@ -36,7 +37,6 @@ public class GUIM : MonoBehaviour {
 				nameHold = null;
 			} else {
 				Invoke ("ReOpen", 0.5f);
-				button_Manger ();
 				nameHold = EventSystem.current.currentSelectedGameObject.gameObject;
 			}
 
@@ -47,48 +47,70 @@ public class GUIM : MonoBehaviour {
 
 	void Same_Button_Close(){
 		infoPanel.GetComponent<UIController> ().Public_Hide ();
+		button_Manager ();
 		Camera.main.transform.position = new Vector3 (laborerFocus.transform.position.x, laborerFocus.transform.position.y, Camera.main.transform.position.z);
 	}
 
 	void ReOpen(){
+		button_Manager ();
 		infoPanel.GetComponent<UIController> ().Play ();
 	}
 
 
-	void button_Manger(){
-		
-		//disable all the button specific info panels
-		for (int i = 0; i < panel_Info_buttons.Count; i++) { 
-			panel_Info_buttons [i].gameObject.SetActive (false);
+	public void clear_Buttons(){
+		for (int i = 0; i < buttons_info.Count; i++) {
+			buttons_info [i].gameObject.SetActive (false);
 		}
+	}
 
-		//Money
+	 public void button_Manager(){
+		print (EventSystem.current.currentSelectedGameObject.name);
+
 		if (EventSystem.current.currentSelectedGameObject.name == "Money") {
-			panel_Info_buttons [0].gameObject.SetActive (true);
+			for (int i = 0; i < buttons_info.Count; i++) {
+				buttons_info [i].gameObject.SetActive (false);
+			}
+			buttons_info [0].gameObject.SetActive (true);
 		}
-
 		//Energy
 		if (EventSystem.current.currentSelectedGameObject.name == "e") {
-			panel_Info_buttons [1].gameObject.SetActive (true);
+			for (int i = 0; i < buttons_info.Count; i++) {
+				buttons_info [i].gameObject.SetActive (false);
+			}
+			buttons_info [1].gameObject.SetActive (true);
 		}
 
 		//Materials
 		if (EventSystem.current.currentSelectedGameObject.name == "m") {
-			panel_Info_buttons [2].gameObject.SetActive (true);
+			for (int i = 0; i < buttons_info.Count; i++) {
+				buttons_info [i].gameObject.SetActive (false);
+			}
+			buttons_info [2].gameObject.SetActive (true);
 		}
 
 		//Employees
 		if (EventSystem.current.currentSelectedGameObject.name == "l") {
-			panel_Info_buttons [3].gameObject.SetActive (true);
+			for (int i = 0; i < buttons_info.Count; i++) {
+				buttons_info [i].gameObject.SetActive (false);
+			}
+			buttons_info [3].gameObject.SetActive (true);
 		}
+
 		//Upgrades
 		if (EventSystem.current.currentSelectedGameObject.name == "u") {
-			panel_Info_buttons [4].gameObject.SetActive (true);
+			for (int i = 0; i < buttons_info.Count; i++) {
+				buttons_info [i].gameObject.SetActive (false);
+			}
+			buttons_info [4].gameObject.SetActive (true);
 		}
 		//Bill of Sale
 		if (EventSystem.current.currentSelectedGameObject.name == "b") {
-			panel_Info_buttons [5].gameObject.SetActive (true);
+			clear_Buttons ();
+			print (buttons_info [5].gameObject.name);
+			buttons_info [5].gameObject.SetActive (true);
 		}
-
+		// Carousel for item
+		// Object Cost
+		//Total Cost area
 	}
 }
---
+++ Assets/Sets/Feb 2017/unit3_GUI/scripts/tutorial.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class tutorial : MonoBehaviour {
+
+	public List<GameObject> buttons = new List<GameObject>();
+	public List<int> tutorial_Stage = new List<int> ();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
